2d266e68a0a42dc28f129588f510f7e2767cd6a9
Kheng Kok Mar <khengkok@gmail.com>
2021-10-26 16:20:10 +0800

    added inference

--- week5/balloon_project/infer_tflite.py
+++ week5/balloon_project/infer_tflite.py
@@ -14,7 +14,7 @@ def load_image_into_numpy_array(path):
       (1, im_height, im_width, 3)).astype(np.float32)
 
 
-img = load_image_into_numpy_array("/home/markk/git/it3103/week5/balloon_project/test_samples/2.png")
+img = load_image_into_numpy_array("/home/markk/git/it3103/week5/balloon_project/test_samples/single.jpg")
 print(img.shape)
 # saved_model_dir = "/home/markk/balloon_project/ssd_mobilenet_v2_320x320_coco17_tpu-8/saved_model"
 tflite_model_dir = "/home/markk/model.tflite"
@@ -25,16 +25,19 @@ interpreter = tf.lite.Interpreter(tflite_model_dir)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+#output_details = interpreter.get_output_details()
 
 interpreter.set_tensor(input_details[0]['index'], img)
 interpreter.invoke()
-print(output_details[0])
-print(output_details[1])
-print(output_details[2])
-print(output_details[3])
 
-names = ['detection scores', "detection boxes", "num boxes", "detection classes" ]
+output_details = interpreter.get_output_details()
+print(len(output_details))
+
+for output_detail in output_details:
+  print(output_detail)
+  print("*"*20)
+
+names = ['output scores', "boxes", "num of detections", "classes" ]
 
 for i in range(4):
   output_data = interpreter.get_tensor(output_details[i]['index'])
@@ -43,6 +46,8 @@ for i in range(4):
 # my_signature = interpreter.get_signature_runner("detect")
 # print(my_signature)
 # my_signature is callable with input as arguments.
-#output = my_signature(x=tf.constant([1.0], shape=(1,10), dtype=tf.float32))
+# output = my_signature(x=tf.constant([1.0], shape=(1,10), dtype=tf.float32))
 # output = my_signature(x=img)
 # print(output)
+
+# print(output_details[0])

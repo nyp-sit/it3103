660334b2658aaae2d00fd184315282b35315c37b
Kheng Kok Mar <khengkok@gmail.com>
2021-11-04 19:38:36 +0800

    modifed to include experiment

--- week5/balloon_project/train.sh
+++ week5/balloon_project/train.sh
@@ -1,7 +1,8 @@
 MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
+EXPERIMENT=run1
 CUDA_VISIBLE_DEVICES=0
-PIPELINE_CONFIG_PATH=/home/ubuntu/balloon_project/models/${MODEL}/pipeline.config
-MODEL_DIR=/home/ubuntu/balloon_project/models/${MODEL}
+PIPELINE_CONFIG_PATH=/home/ubuntu/balloon_project/models/${MODEL}/${EXPERIMENT}/pipeline.config
+MODEL_DIR=/home/ubuntu/balloon_project/models/${MODEL}/${EXPERIMENT}
 
 python model_main_tf2.py \
     --pipeline_config_path="${PIPELINE_CONFIG_PATH}" \

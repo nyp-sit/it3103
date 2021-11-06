40360fdad016e8eee10ef67db8c7ce7f70e14ed6
Kheng Kok Mar <khengkok@gmail.com>
2021-11-06 23:31:38 +0800

    Update paths

--- week5/balloon_project_colab/export.sh
+++ week5/balloon_project_colab/export.sh
@@ -1,9 +1,9 @@
 MODEL=ssd_mobilenet_v2_320x320_coco17_tpu-8
 EXPERIMENT=run1
-PIPELINE_CONFIG_PATH=/home/ubuntu/balloon_project/models/${MODEL}/${EXPERIMENT}/pipeline.config
-MODEL_DIR=/home/ubuntu/balloon_project/models/${MODEL}/${EXPERIMENT}/
-TRAIN_CHECKPOINT_DIR=/home/ubuntu/balloon_project/models/${MODEL}/${EXPERIMENT}/
-EXPORT_DIR=/home/ubuntu/balloon_project/exported_models/${MODEL}/${EXPERIMENT}/
+PIPELINE_CONFIG_PATH=/drive/balloon_project/models/${MODEL}/${EXPERIMENT}/pipeline.config
+MODEL_DIR=/drive/balloon_project/models/${MODEL}/${EXPERIMENT}/
+TRAIN_CHECKPOINT_DIR=/drive/balloon_project/models/${MODEL}/${EXPERIMENT}/
+EXPORT_DIR=/drive/balloon_project/exported_models/${MODEL}/${EXPERIMENT}/
 
 python exporter_main_v2.py \
     --input_type image_tensor \
